e0728a4e489358140554fabbb9f347e402968665
Andy Bohne <andy@andrewbohne.com>
2017-09-28 01:08:23 -0400

    Manipulated our string to parse correctly with sexpistol

--- frontline.rb
+++ frontline.rb
@@ -2,5 +2,11 @@ require 'sexpistol'
 
 str = "(id,created,employee(id,firstname,employeeType(id), lastname),location)"
 
+# remove initial whitespace
+str.gsub!(' ', '')
+
+# s expressions use spaces to delimit items
+str.gsub!(',', ' ')
+
 @parser = Sexpistol.new
 result = @parser.parse_string(str)
